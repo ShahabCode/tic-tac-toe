768f226a78ffe6c8b8fcae92c38355896abeb547
ShahabCode <shahabepic@gmail.com>
2025-11-25 09:24:00 -0700

    Add function to display board with colored X and O

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+from termcolor import colored
+
 board = list(range(1, 10))
 winners = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6))
 moves = ((1, 3, 7, 9), (5,), (2, 4, 6, 8))
+
+
+def print_board():
+    j = 1
+    for i in board:
+        end = " "
+        if j % 3 == 0:
+            end = "\n\n"
+        if i == "X":
+            print(colored(f"[{i}]", "red"), end=end)
+        elif i == "O":
+            print(colored(f"[{i}]", "blue"), end=end)
+        else:
+            print(f"[{i}]", end=end)
+        j += 1
